3a205c8e179dfeeb6e09aaf4e7b3ea96414ddad2
Hiromi <hiromis@gmail.com>
2019-09-07 00:03:55 -0500

    Simplifying test.sh

--- test.sh
+++ test.sh
@@ -2,18 +2,8 @@
 
 cd dev
 
-function onExit {
-    if [ "$?" != "0" ]; then
-        echo "Test failure";
-        echo "$output"
-        exit 1;
-    fi
-}
-
-trap onExit EXIT;
-
 for i in {0,1,2}*.ipynb
 do
-    echo $i
     output=$((python run_notebook.py --fn $i) 2>&1)
+    echo "$output"
 done

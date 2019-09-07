7243bc45ea70139404867bc982e78d889aab915f
Hiromi <hiromis@gmail.com>
2019-09-07 12:15:12 -0500

    just saving

--- test.sh
+++ test.sh
@@ -8,8 +8,9 @@ for i in {0,1,2}*.ipynb
 do
     output=$((python run_notebook.py --fn $i) 2>&1)
     echo "$output"
-    if [[ $string == *"Error "* ]]; then
+    if [[ $output == *"Error"* ]]; then
         return_code=1
     fi
 done
+echo $return_code
 exit $return_code;

1abf0c0f4c730a132774808c9d9b3b1a5b7f29bd
Hiromi <hiromis@gmail.com>
2019-09-07 07:54:18 -0500

    Adding trap back in as it exits witn lack of CUDA

--- test.sh
+++ test.sh
@@ -2,6 +2,15 @@
 
 cd dev
 
+function onExit {	
+    if [ "$?" != "0" ]; then	
+        echo "Test failure";
+        exit 1;	
+    fi	
+}	
+
+trap onExit EXIT;
+
 for i in {0,1,2}*.ipynb
 do
     output=$((python run_notebook.py --fn $i) 2>&1)

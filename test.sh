8a001c4b054005984e493dbd4f346b5253f5d28e
Hiromi <hiromis@gmail.com>
2019-09-07 08:47:07 -0500

    trying to catch error

--- test.sh
+++ test.sh
@@ -2,19 +2,14 @@
 
 cd dev
 
-function onExit {	
-    if [ "$?" != "0" ]; then	
-        echo "Test failure";
-        echo "$output"
-        exit 1;
-    else
-       echo "$output"
-    fi	
-}	
-
-trap onExit EXIT;
+return_code=0
 
 for i in {0,1,2}*.ipynb
 do
     output=$((python run_notebook.py --fn $i) 2>&1)
+    echo "$output"
+    if [[ $string == *"Error "* ]]; then
+        return_code=1
+    fi
 done
+exit $return_code;
